b7486cdef8ca6bcc15744333c549c968d9dc6786
wangym <908289704@qq.com>
2022-02-18 16:30:24 +0800

    refactor: request errorhandle logic

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,6 +7,9 @@ module.exports = merge(common, {
   devServer: {
     port: '9000',
     hot: true,
+    // 使用ngrok内网穿透只返回304，invalid host header,
+    // 新版的webpack-dev-server出于安全考虑，默认检查hostname，如果hostname不是配置内的，将中断访问
+    allowedHosts: 'all',
     // host: '192.168.43.79',
   },
 });
